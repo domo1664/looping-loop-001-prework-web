aa42acf7218eb5befb8d69d0e84b050d9d9a8f78
domo1664 <domo1664@gmail.com>
2016-05-23 13:39:26 -0400

    Done.

--- looping.rb
+++ looping.rb
@@ -1,10 +1,11 @@
-def looping
-	#your code here
+def looping_
+	#created a loop
   loop do
     puts "Wingardium Leviosa"
   end
 end
 
 
-#call your method here
-looping
+#calling method looping
+looping_
+#changed method due to error saying its the same and to make changes
